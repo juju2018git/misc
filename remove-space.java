cbde9dea2f9d89c87e2df08d9be14ab5fca5943d
juju2018git <39884341+juju2018git@users.noreply.github.com>
2019-06-07 23:08:39 -0700

    Create remove-space.java

---
+++ remove-space.java
@@ -0,0 +1,24 @@
+// in : ___abc____cd__ef
+// out: abc_cd_ef
+public void removeSpaces(char[] s) {
+    // slow: on the left hand side of slow, not including slow, are final answers
+    // fast: current pointer
+    int slow = 0, fast = 0, n = s.length;
+    int wordCount = 0;  // special case for 1st word
+    while (true) {
+        while (fast < n && s[fast] == ' ') {
+            ++fast;  // 1. skip all leading ' ' in front of each word
+        }
+        if (fast == n) {
+            break;
+        }
+        if (wordCount > 0) {
+            s[slow++] = ' ';  // 2. add ' ' in front of (2nd+) word
+        }
+        while (fast < n && s[fast] != ' ') {
+            s[slow++] = s[fast++];  // 3. copy a word
+        }
+        ++wordCount;
+    }
+    s.resize(slow);
+}
